68d9a6e6ae076eadf83804ffb2ac79e490a54560
xue <xuefenglei04@126.com>
2024-06-01 23:13:39 +0800

    update

--- date/simple.go
+++ date/simple.go
@@ -3,6 +3,7 @@ package date
 import "time"
 
 var SimpleDateFormat = "2006-01-02 15:04:05"
+var SimpleDate = "2006-01-02"
 
 func Sprint(date time.Time, format string) string {
 	return date.Format(format)
